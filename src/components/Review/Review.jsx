bf9082a386fc0607d12e1ec6d96dc276ab9a3bea
Joe AB <joe.anthonybrown@gmail.com>
2022-05-06 23:03:00 -0500

    all question forms contribute to the final review, and when submitted all values are stored in a table

--- src/components/Review/Review.jsx
+++ src/components/Review/Review.jsx
@@ -2,34 +2,46 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { useDispatch } from 'react-redux';
 import { useHistory } from 'react-router-dom';
+import axios from 'axios';
 
 
-function Review(){
+function Review() {
 
-    const dispatch = useDispatch();
-    const history = useHistory();
+  const dispatch = useDispatch();
+  const history = useHistory();
 
-    const feeling = useSelector(store => store.feelingReducer);
-    const understanding = useSelector(store => store.understandingReducer);
-    const support = useSelector(store => store.supportReducer);
-    const comments = useSelector(store => store.commentsReducer);
+  const feeling = useSelector(store => store.feelingReducer);
+  const understanding = useSelector(store => store.understandingReducer);
+  const support = useSelector(store => store.supportReducer);
+  const comments = useSelector(store => store.commentsReducer);
 
-    function handleSubmit() {
-      history.push('/done')
+  function handleSubmit() {
+    console.log( feeling, understanding, support, comments );
+    axios({
+      method: 'POST',
+      url: '/formSubmission',
+      data: { feeling, understanding, support, comments }
+    }).then((response) => {
+      console.log('response from review submit button:', response);
+      history.push('/done');
+    }).catch((error) => {
+      console.log('POST /formSubmission broke:', error);
+    })
 
-    }
 
-    return(
-        <div id="review">
-        <h1>Review Your Feedback</h1>
-        <h3>Feelings:{feeling}</h3>
-        <h3>Understanding:{understanding}</h3>
-        <h3>Support:{support}</h3>
-        <h3>Comments:{comments}</h3>
-        <button onClick={handleSubmit}>SUBMIT</button>
-      </div>
-    )
-    
+  }
+
+  return (
+    <div id="review">
+      <h1>Review Your Feedback</h1>
+      <h3>Feelings: {feeling}</h3>
+      <h3>Understanding: {understanding}</h3>
+      <h3>Support: {support}</h3>
+      <h3>Comments: {comments}</h3>
+      <button onClick={handleSubmit}>SUBMIT</button>
+    </div>
+  )
+
 }
 
 export default Review
